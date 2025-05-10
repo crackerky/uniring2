1a9e05561ec0576400fb89cab51a302ef584b71c
crackerky <crackert.ky@gmail.com>
2025-05-11 00:15:24 +0900

    Fix API routes for static export compatibility

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9267dd572387a979"; }
+/******/ 		__webpack_require__.h = function() { return "af2f7b7e9b334ac3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
